00d053af3446cb5e24643f3904400b9379c03001
jose-donato <43375532+jose-donato@users.noreply.github.com>
2024-03-24 19:22:08 +0000

    adds tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import deno from "@astrojs/deno";
+import tailwind from "@astrojs/tailwind";
 import { defineConfig, passthroughImageService } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
 	output: "server",
+	integrations: [tailwind()],
 	adapter: deno(),
 	image: {
 		service: passthroughImageService(),
