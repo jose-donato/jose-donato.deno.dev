bdf5c88ec8e96d05fdd1ca0abe301f4e8ce862f6
jose-donato <43375532+jose-donato@users.noreply.github.com>
2025-03-05 22:23:56 +0000

    installing new dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 import deno from "@astrojs/deno";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, passthroughImageService } from "astro/config";
 
 export default defineConfig({
 	output: "server",
-	integrations: [tailwind()],
 	adapter: deno(),
 	image: {
 		service: passthroughImageService(),
 	},
 	devToolbar: {
 		enabled: false,
+	},
+	vite: {
+		plugins: [tailwindcss()],
 	},
 });
